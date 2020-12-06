99f6097a0341c29d2372dda0bb9309ceea2bc5f1
Evis Chang <evis.chang@seidor.es>
2020-12-06 12:07:05 +0800

    update the oracle driver

--- src/main/java/com/spgg/blog/ServletInitializer.java
+++ src/main/java/com/spgg/blog/ServletInitializer.java
@@ -1,8 +1,11 @@
 package com.spgg.blog;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
